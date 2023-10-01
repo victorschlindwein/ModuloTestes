626999f58cfd6f3e707d2ec86b35f2931a4bd254
dizzysch <victorwilbert@gmail.com>
2023-10-01 10:38:11 -0300

    feat: adiciona dois modelos de implesmentacao

---
+++ Calculadora/Services/ValidacoesString.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class ValidacoesString
+    {
+        public int ContaCaracteres(string text)
+        {
+            int num = text.Length;
+            return num;
+        }
+    }
+}
